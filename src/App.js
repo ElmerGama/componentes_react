3099bb00b279ff163e687dce2936bec2c7f1fa82
ElmerGama <43096999+ElmerGama@users.noreply.github.com>
2024-10-19 03:35:21 +0000

    Git inicial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Pepillo from './pepillo';
+
+function App() {
+  return (
+    <div className="App-pepillo">
+      <h1>
+        Hola mundo
+      </h1>
+      <Pepillo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pepillo.js
@@ -0,0 +1,9 @@
+import React from "react";
+function Pepillo(){
+    return(
+        <div className="App-pepillo">
+            <h2>componente pepillo</h2>
+        </div>
+    );
+}
+export default Pepillo;
